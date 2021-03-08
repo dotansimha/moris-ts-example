08491b28b9d9cc661f96f7dc48129ec9bd0dd92b
Dotan Simha <dotansimha@gmail.com>
2021-03-08 21:52:16 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// here you can do all the cool shit, like import:
+import { readFileSync } from "fs";
+
+// And this works
+import { CompanyTypes, createScraper } from "israeli-bank-scrapers";
+
+console.log("Hi!");
